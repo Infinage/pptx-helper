996ec4c4c9f80c7fb191e2f021c41e7a782e6810
infinage <37257700+Infinage@users.noreply.github.com>
2024-09-16 23:00:14 +0530

    Table support

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pptx
 import typing
 import util.Helper
+import importlib
+
+# Reload - Helper util
+importlib.reload(util.Helper)
 
 helper = util.Helper.PPTHelper("/mnt/c/Users/kael/Desktop/test.pptx", "target")
 helper.to_html()
--- util/Helper.py
+++ util/Helper.py
@@ -1,5 +1,6 @@
 import pptx
 import os
+import shutil
 
 class PPTHelper:
 
@@ -27,6 +28,10 @@ class PPTHelper:
         self.slide_height = PPTHelper.emu_to_px(self.presentation.slide_height or 0)
 
     def to_html(self) -> None:
+
+        # Copy style.css
+        shutil.copy2("style.css", os.path.join(self.output_dpath, "style.css"))
+
 
         # Index.html
         with open(os.path.join(self.output_dpath, "index.html"), "w") as f:
@@ -58,6 +63,13 @@ class PPTHelper:
                 elif shape.has_table:
                     markup = self.table_to_fragment(slide_id, shape_id)
 
+                    # Generate fragment for each cell's textframe
+                    for row_num, row in enumerate(shape.table.rows):
+                        for col_num, cell in enumerate(row.cells):
+                            cell_fragment = self.cell_text_frame_to_fragment(slide_id, shape_id, row_num, col_num)
+                            with open(os.path.join(self.output_dpath, f"frag_{slide_id}_{shape_id}_({row_num},{col_num}).html"), "w") as f:
+                                f.write(f"<html><head><link rel='stylesheet' href='style.css'></head>\n{cell_fragment}</html>")
+
                 else:
                     markup = f"Sorry, shape type: '{shape.shape_type}' not supported yet."
 
@@ -81,7 +93,7 @@ class PPTHelper:
 
             # Create a rectangle for each shape
             shape_html = (
-                f"<a class='fragment-link' href='frag_{slide_id}_{shape_id}.html'>"
+                f"<a class='nav-link' href='frag_{slide_id}_{shape_id}.html'>"
                 f'<div class="shape" style="left:{left}px; top:{top}px; width:{width}px; height:{height}px;">\n'
                 f'<p>Shape ID: {shape_id}<br>Type: {shape.shape_type}</p>\n'
                 '</div></a>\n'
@@ -89,12 +101,43 @@ class PPTHelper:
 
             slide_html += shape_html
 
-        slide_html += f'<div class="slide-footer"><p class="slide-id">Slide ID: {slide_id}</p></div>\n</html>'
+        slide_html += (
+            f'<div class="slide-footer">'
+                f'<a href="index.html" class="nav-link">'
+                    f'<p class="slide-id">Slide ID: {slide_id}</p>'
+                f'</a>'
+            f'</div></html>'
+        )
         return slide_html
 
     def text_frame_to_fragment(self, slide_id: int, shape_id: int) -> str:
-        frag = f"<div class='text-frame'><p class='slide-shape-id'>Slide / Shape ID: {slide_id}, {shape_id}</p>"
+        frag = (
+            f"<div class='text-frame'><p class='slide-shape-id'>"
+                f"<a href='index.html' class='nav-link'>Slide ID: {slide_id}</a> / "
+                f"<a href='slide_{slide_id}.html' class='nav-link'>Shape ID: {shape_id}</a>"
+            f"</p>"
+        )
         for para_id, para in enumerate(self.presentation.slides[slide_id].shapes[shape_id].text_frame.paragraphs):
+            frag += f"<div class='text-frame-para'><p class='para-id'>Para ID: {para_id}</p>"
+            for run_id, run in enumerate(para.runs):
+                frag += f"<div class='text-frame-run'><p class='run-id'>Run ID: {run_id}</p><p class='run-text'>{run.text}</p></div>"
+            frag += "</div>"
+        frag += "</div>"
+        return frag
+
+    def cell_text_frame_to_fragment(self, slide_id: int, shape_id: int, row_num: int, col_num: int) -> str:
+        frag = (
+            f"<div class='text-frame'>"
+                "<p class='slide-shape-id'>"
+                    f"<a href='index.html' class='nav-link'>Slide ID: {slide_id}</a> / "
+                    f"<a href='slide_{slide_id}.html' class='nav-link'>Shape ID: {shape_id}</a>"
+                "</p>"
+                "<p class='slide-shape-id'>"
+                    f"<a href='frag_{slide_id}_{shape_id}.html' class='nav-link'>Row / Col: {shape_id}, {col_num}</a>"
+                "</p>"
+            f"</p>"
+        )
+        for para_id, para in enumerate(self.presentation.slides[slide_id].shapes[shape_id].table.cell(row_num, col_num).text_frame.paragraphs):
             frag += f"<div class='text-frame-para'><p class='para-id'>Para ID: {para_id}</p>"
             for run_id, run in enumerate(para.runs):
                 frag += f"<div class='text-frame-run'><p class='run-id'>Run ID: {run_id}</p><p class='run-text'>{run.text}</p></div>"
@@ -105,9 +148,25 @@ class PPTHelper:
     def table_to_fragment(self, slide_id: int, shape_id: int) -> str:
         table = self.presentation.slides[slide_id].shapes[shape_id].table
         frag = (
-            f"<div class='table'>"
-            f"<p class='slide-shape-id'>Slide / Shape ID: {slide_id}, {shape_id}</p>"
+            f"<div class='table-div'>"
+                "<p class='slide-shape-id'>"
+                    f"<a href='index.html' class='nav-link'>Slide ID: {slide_id}</a> / "
+                    f"<a href='slide_{slide_id}.html' class='nav-link'>Shape ID: {shape_id}</a>"
+                f"</p>"
+            f"<table>"
         )
-        # TODO: Complete function
-        frag += "</div>"
+
+        for row_num, row in enumerate(table.rows):
+            frag += "<tr>"
+            for col_num, cell in enumerate(row.cells):
+                if not cell.is_spanned:
+                    frag += (
+                        f"<td colspan='{cell.span_width}' rowspan='{cell.span_height}'>"
+                            f"<a class='nav-link' href='frag_{slide_id}_{shape_id}_({row_num},{col_num}).html'>"
+                            f"<div class='table-cell-text'>{cell.text}</div>"
+                        "</td>"
+                    )
+            frag += "</tr>"
+
+        frag += "</table></div>"
         return frag
